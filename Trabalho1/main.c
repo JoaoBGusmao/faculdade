1e76a756d684d999bd2202ed4e57478cd2afef3c
JoaoBGusmao <joao@natsce.com>
2015-10-21 20:34:24 -0200

    Adicionando post ainda. Com categoria e autor

--- Trabalho1/main.c
+++ Trabalho1/main.c
@@ -8,7 +8,7 @@
 //Cadatrar novos registros
 
 //PT2:
-
+//Usar funções
 void SetColorAndBackground(int ForgC, int BackC)
 {
      WORD wColor = ((BackC & 0x0F) << 4) + (ForgC & 0x0F);
@@ -20,11 +20,11 @@ void limpaTeclado() {
 	fflush(stdin);
 }
 
-typedef struct Categoria {
-	char nome[50];
-	int idade;
-	char about[250];
-} Categoria;
+typedef struct Numeros {
+	int Npostagens = 0;
+	int Nautores = 0;
+	int Ncat = 0;
+} Numeros;
 
 typedef struct Autor {
 	char nome[50];
@@ -38,14 +38,13 @@ typedef struct Data {
 	int ano;
 } Data;
 
-
 typedef struct Postagens {
 	char titulo[250];
 	char resumo[250];
 	char conteudo[250];
+	char categoria[100];
 	Data data;
 	Autor autor;
-	Categoria categoria;
 } Postagens;
 
 int getElemNum(Postagens *posts) {
@@ -58,25 +57,81 @@ int getElemNum(Postagens *posts) {
 	return 0;
 }
 
-void addPost(Postagens *posts) {
+void showAuthorList(Autor *autores, Numeros *numeros) {
+	int i = 0;
+	printf("ID      Nome            Idade\n");
+	for(i=0;i < numeros->Nautores;i++) {
+		printf("%d    %-20s %d\n",i,autores[i].nome,autores[i].idade);
+	}
+}
+
+void showCatList(char categorias[100][100], Numeros *numeros) {
+	int i = 0;
+	printf("ID      Categoria\n");
+	for(i=0;i < numeros->Ncat;i++) {
+		printf("%d      %s\n",i,categorias[i]);
+	}
+}
+
+void addPost(Postagens *posts, Autor *autores,char categorias[100][100],Numeros *numeros) {
 	system("cls");	
 	SetColorAndBackground(0,0xFFF);
 	printf("Digite o título da postagem:\n");
 	SetColorAndBackground(0xFFF,0);
 	limpaTeclado();
 	scanf("%[^\n]s",posts[getElemNum(posts)].titulo);
+	
 	SetColorAndBackground(0,0xFFF);
 	printf("\nResuma sua postagem:\n");
 	SetColorAndBackground(0xFFF,0);
 	limpaTeclado();
 	scanf("%[^\n]s",posts[getElemNum(posts)].resumo);
 	
+	SetColorAndBackground(0,0xFFF);
+	printf("\nDigite o ID do autor:\n");
+	SetColorAndBackground(0xFFF,0);
+	showAuthorList(autores,numeros);
+	limpaTeclado();
+	int autorId = 0;
+	scanf("%d",&autorId);
+	posts[getElemNum(posts)].autor = autores[autorId];
+	
+	SetColorAndBackground(0,0xFFF);
+	printf("\nDigite o ID da categoria:\n");
+	SetColorAndBackground(0xFFF,0);
+	showCatList(categorias,numeros);
+	limpaTeclado();
+	int catId = 0;
+	scanf("%d",&catId);
+	//strcpy(posts[getElemNum(posts)].categoria, categorias[catId]);
+	
+	numeros->Npostagens++;
+	
+}
+
+void addPrePosts(Autor *autores, char categorias[100][100],Numeros *numeros) {
+	strcpy(autores[0].nome,"Joao Gusmao");
+	autores[0].idade = 19;
+	strcpy(autores[0].about,"Sobre eu");
+	
+	strcpy(autores[1].nome,"Jao dasmao");
+	autores[1].idade = 21;
+	strcpy(autores[1].about,"Sobre eu2");
+	numeros->Nautores = 2;
+	
+	strcpy(categorias[0],"Noticias");
+	strcpy(categorias[1],"Entretenimento");
+	numeros->Ncat = 2;
 }
 
 int main() {
 	SetColorAndBackground(0xFFF,0);
 	Postagens posts[100];
+	Autor autores[100];
+	char categorias[100][100];
+	Numeros numeros;
 	int opcao;
+	addPrePosts(autores,categorias,&numeros);
 	while(true) {
 		opcao=0;
 		system("cls");
@@ -85,11 +140,11 @@ int main() {
 		printf("2 - Visualizar Postagens\n");
 		printf("3 - Adicionar Categoria\n");
 		printf("4 - Adicionar Autor\n");
-		
+		printf("Numero de post: %d",numeros.Npostagens);
 		printf("\n\n\nEscolha um menu:");
 		scanf("%d",&opcao);
 		if(opcao == 1) {
-			addPost(posts);
+			addPost(posts,autores,categorias,&numeros);
 		}
 	}
 }
